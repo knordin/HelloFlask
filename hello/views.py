a0bb1f305eed675dccb426073fd114351943e9b8
oracle <oracle@bigdatalite.localdomain>
2014-11-20 19:16:46 -0500

    GET-ing and POST-ing db json data now

--- hello/views.py
+++ hello/views.py
@@ -7,16 +7,12 @@ import datetime
 @app.route('/', methods=['GET', 'POST'])
 def index():
    # request.values is where the json for you to access
-   # print dir(request)
-    print request.values 
 
-    form = CommentForm()
-    if form.validate_on_submit():
-        comment = Profile(
-            request.values
-        )
-        db.session.add(comment)
-        db.session.commit()
-        return redirect(url_for('index'))
+    data = request.values.keys()
+    if len(data)>0:
+	print data[0]
+    	db.session.add(Profile(data[0]))
+	db.session.commit()
+    	return redirect(url_for('index'))
     comments = Profile.query.order_by(db.desc(Profile.comment_id))
-    return render_template('index.html', comments=comments, form=form)
+    return render_template('index.html', comments=comments)
