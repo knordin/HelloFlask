79df5ebc140aef94e1620928df1dfdf8dd830834
oracle <oracle@bigdatalite.localdomain>
2014-12-08 13:19:11 -0500

    added test data sql script

---
+++ hello/insert_test_user_data.sql
@@ -0,0 +1,13 @@
+INSERT INTO "user" (user_id, username, "user".password)
+VALUES (1, 'John', 'John');
+
+INSERT INTO "user" (user_id, username, "user".password)
+VALUES (2, 'Jane', 'Jane');
+
+commit;
+
+
+
+
+
+
