ff83930394a7966e7173689905b4a0c586e9adad
oracle <oracle@bigdatalite.localdomain>
2014-12-18 18:30:33 -0500

    fixed some style in search

--- hello/insert_test_user_data.sql
+++ hello/insert_test_user_data.sql
@@ -7,3 +7,9 @@ truncate table user_profile;
 insert into user_profile (comment_id, doc) values (1, '{"Profname":"john", "username":"john", "age":22}'); 
 insert into user_profile (comment_id, doc) values (2, '{"Profname":"jane", "username":"jane", "age":23}'); 
 commit;
+
+/* insert a third user example */
+
+insert into "user" (user_id, username, password) values (3, 'jack', 'doe');
+insert into user_profile (comment_id, doc) values (3, '{"Profname":"Jack Doe", "username":"jack", "age":27}');
+commit;
